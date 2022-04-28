477c3727de49b58f48b707d34365c7071595d4ed
Giga Marsagishvili <nofacemarr@gmail.com>
2022-04-28 13:01:20 +0400

    master: create bank of georgia payment payum factory and define form types for configuration in admin panel

---
+++ src/Form/Type/BankOfGeorgiaGatewayConfigurationType.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gigamarr\SyliusBankOfGeorgiaPlugin\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class BankOfGeorgiaGatewayConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('client_id', TextType::class);
+    }
+}
