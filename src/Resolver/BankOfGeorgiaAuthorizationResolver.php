9dc71d8431a35d407346d4e00cd3f341bff39132
Giga Marsagishvili <nofacemarr@gmail.com>
2022-04-29 19:50:16 +0400

    master: yml -> yaml

---
+++ src/Resolver/BankOfGeorgiaAuthorizationResolver.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gigamarr\SyliusBankOfGeorgiaPlugin\Resolver;
+
+final class BankOfGeorgiaAuthorizationResolver
+{
+    public function resolve($object): void {
+        $testName = 'giga';
+    }
+}
