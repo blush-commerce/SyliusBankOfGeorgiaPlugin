e9a4c6b915efda02aa95e2490487a16310cbc0c1
Giga Marsagishvili <nofacemarr@gmail.com>
2022-04-30 15:27:27 +0400

    master: switch service id's to FQCNs following symfony service naming standards

--- src/Processor/AuthorizationProcessor.php
+++ src/Processor/AuthorizationProcessor.php
@@ -7,12 +7,14 @@ namespace Gigamarr\SyliusBankOfGeorgiaPlugin\Processor;
 use Gigamarr\SyliusBankOfGeorgiaPlugin\Client\BankOfGeorgiaClient;
 use Sylius\Component\Core\Model\OrderInterface;
 use Payum\Core\Payum;
+use Psr\Log\LoggerInterface;
 
 final class AuthorizationProcessor implements ProcessorInterface
 {
     public function __construct(
         private Payum $payum,
-        private BankOfGeorgiaClient $bankOfGeorgiaClient
+        private BankOfGeorgiaClient $bankOfGeorgiaClient,
+        private LoggerInterface $logger
     )
     {
     }
