72fe1ee016714bdf8adb68684ddf4459dfdedcd0
Giga Marsagishvili <nofacemarr@gmail.com>
2022-04-30 11:14:39 +0400

    master: inject authorization resolver with payum

--- src/Resolver/AuthorizeResolver.php
+++ src/Resolver/AuthorizeResolver.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace Gigamarr\SyliusBankOfGeorgiaPlugin\Resolver;
 
+use Sylius\Component\Core\Model\OrderInterface;
+use Payum\Core\Payum;
+
 final class AuthorizeResolver
 {
-    public function resolve($object): void {
-        $testName = 'giga';
+    public function __construct(private Payum $payum)
+    {
+    }
+
+    public function resolve(OrderInterface $order): void {
+        // TODO: execute authorize action and redirect user
     }
 }
