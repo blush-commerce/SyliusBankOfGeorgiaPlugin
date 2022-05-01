03c638f89b6fb39c6eb8dbf8f94615f8b29637f3
Giga Marsagishvili <nofacemarr@gmail.com>
2022-05-01 19:33:21 +0400

    master: use class constants instead of enums to revert back to php 8.0

--- src/Formatter/OrderToAuthorizeActionPayloadFormatter.php
+++ src/Formatter/OrderToAuthorizeActionPayloadFormatter.php
@@ -11,22 +11,39 @@ use Sylius\Component\Core\Model\OrderInterface;
 
 final class OrderToAuthorizeActionPayloadFormatter
 {
+    private const INTENT = [
+        'CAPTURE'   => 'CAPTURE',
+        'AUTHORIZE' => 'AUTHORIZE',
+    ];
+
+    private const CAPTURE_METHOD = [
+        'AUTOMATIC' => 'AUTOMATIC',
+        'MANUAL'    => 'MANUAL',
+    ];
+
+    private const CURRENCY_CODE = [
+        'GEL' => 'GEL',
+        'USD' => 'USD',
+        'EUR' => 'EUR',
+        'GBP' => 'GBP',
+    ];
+
     public function format(OrderInterface $order): array
     {
         $orderItems = $this->formatOrderItems($order);
 
         $payload = [
-            'intent' => Intent::CAPTURE, // TODO: make this configurable from the backend
+            'intent' => self::INTENT['CAPTURE'], // TODO: make this configurable from the backend
             'items' => $orderItems,
             'locale' => 'ka', // TODO: set this based on current locale context
             'shop_order_id' => (string) $order->getId(),
             'redirect_url' => 'https://blush.ge', // TODO: make this configurable from the backend
             'show_shop_order_id_on_extract' => TRUE, // TODO: make this configurable from the backend
-            'capture_method' => CaptureMethod::AUTOMATIC, // TODO: make this configurable from the backend
+            'capture_method' => self::CAPTURE_METHOD['AUTOMATIC'], // TODO: make this configurable from the backend
             'purchase_units' => [
                 [
                     'amount' => [
-                        'currency_code' => CurrencyCode::GEL, // TODO: make this configurable from the backend
+                        'currency_code' => self::CURRENCY_CODE['GEL'], // TODO: make this configurable from the backend
                         'value' => number_format(abs($order->getTotal()) / 100, 2)
                     ]
                 ]
