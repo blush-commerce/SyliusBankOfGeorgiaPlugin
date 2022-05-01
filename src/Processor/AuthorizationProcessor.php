a504a7465b0657029cf49f4d303772cb66cebc66
Giga Marsagishvili <nofacemarr@gmail.com>
2022-05-01 11:39:40 +0400

    master: remove bank of georgia client service from authorization processor

--- src/Processor/AuthorizationProcessor.php
+++ src/Processor/AuthorizationProcessor.php
@@ -4,21 +4,18 @@ declare(strict_types=1);
 
 namespace Gigamarr\SyliusBankOfGeorgiaPlugin\Processor;
 
-use Gigamarr\SyliusBankOfGeorgiaPlugin\Client\BankOfGeorgiaClient;
 use Sylius\Component\Core\Model\OrderInterface;
 use Payum\Core\Payum;
 
 final class AuthorizationProcessor implements ProcessorInterface
 {
     public function __construct(
-        private Payum $payum,
-        private BankOfGeorgiaClient $bankOfGeorgiaClient,
+        private Payum $payum
     )
     {
     }
 
     public function process(OrderInterface $order): void
     {
-        $auth = $this->bankOfGeorgiaClient->authenticate();
     }
 }
