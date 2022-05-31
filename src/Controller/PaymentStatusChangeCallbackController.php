373070c0de8a92f85f0f6c5f6f94f704427a46a0
Giga Marsagishvili <nofacemarr@gmail.com>
2022-05-31 14:35:11 +0400

    master: verify payment_hash before proceeding with the order

--- src/Controller/PaymentStatusChangeCallbackController.php
+++ src/Controller/PaymentStatusChangeCallbackController.php
@@ -5,15 +5,38 @@ declare(strict_types=1);
 namespace Gigamarr\SyliusBankOfGeorgiaPlugin\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Sylius\Component\Core\Model\Order;
+use Sylius\Component\Core\Model\PaymentInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 final class PaymentStatusChangeCallbackController
 {
-    public function __construct()
+    public function __construct(
+        private RepositoryInterface $orderRepository
+    )
     {
     }
 
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
-        return new Response(null, 200);
+        /** @var Order $order */
+        $order = $this->orderRepository->findOneBy(
+            ['id' => $request->get('shop_order_id')]
+        );
+        
+        // TODO: check that payment uses BOG gateway
+        if ($order) {
+            $paymentDetails = $order->getLastPayment()->getDetails();
+
+            if ($request->get('payment_hash') == $paymentDetails['payment_hash']) {
+                return new Response(null, 200);
+            }
+
+
+            return new Response(null, 401);
+        }
+
+        return new Response(null, 404);
     }
 }
